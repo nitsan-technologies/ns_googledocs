0fa3fe56fedc49dc16bdb9c04a2fc206a7c643f1
NITSAN Technologies <sanjay@nitsan.in>
2022-08-09 15:09:11 +0530

    [TASK] Change URL of t3terminal into t3planet

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,7 +2,7 @@
 
 $EM_CONF['ns_googledocs'] = [
     'title' => '[NITSAN] Import Google Docs to TYPO3',
-    'description' => 'First-ever feature-rich TYPO3 solution to one-click import your Google Docs to your TYPO3 pages, news and blog. Demo: https://demo.t3terminal.com/t3t-extensions/ns-google-docs PRO: http://t3terminal.com/ns-google-docs-typo3-extension',
+    'description' => 'First-ever feature-rich TYPO3 solution to one-click import your Google Docs to your TYPO3 pages, news and blog. Demo: https://demo.t3planet.com/t3t-extensions/ns-google-docs PRO: http://t3planet.com/ns-google-docs-typo3-extension',
     'category' => 'module',
     'author' => 'NITSAN Technologies Pvt Ltd',
     'author_email' => 'sanjay@nitsan.in',
